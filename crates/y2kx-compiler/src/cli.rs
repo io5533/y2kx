4af413ae1465c83740d6bfb6b6014e125794ac73
io5533 <io5533@users.noreply.github.com>
2026-08-01 13:23:16 +0900

    feat: code y2kx-compiler

---
+++ crates/y2kx-compiler/src/cli.rs
@@ -0,0 +1,55 @@
+use clap::{Parser, ValueEnum};
+use y2kx::{CompileOptions, NoteOrder};
+
+#[derive(Clone, Copy, Debug, ValueEnum)]
+pub enum CliNoteOrder {
+    LowToHigh,
+    HighToLow,
+    Random,
+}
+
+impl From<NoteOrder> for CliNoteOrder {
+    fn from(value: NoteOrder) -> Self {
+        match value {
+            NoteOrder::LowToHigh => CliNoteOrder::LowToHigh,
+            NoteOrder::HighToLow => CliNoteOrder::HighToLow,
+            NoteOrder::Random => CliNoteOrder::Random,
+        }
+    }
+}
+
+impl From<CliNoteOrder> for NoteOrder {
+    fn from(value: CliNoteOrder) -> Self {
+        match value {
+            CliNoteOrder::LowToHigh => Self::LowToHigh,
+            CliNoteOrder::HighToLow => Self::HighToLow,
+            CliNoteOrder::Random => Self::Random,
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// input: MIDI (mid)
+    pub input: String,
+
+    /// output: Y2KXq (y2kx)
+    pub output: String,
+
+    /// Time in milliseconds to wait before starting playback.
+    #[arg(short, long, default_value_t = CompileOptions::default().preparation_time)]
+    pub prepare: u16,
+
+    /// Interval in milliseconds between notes in the same chord.
+    #[arg(short, long, default_value_t = CompileOptions::default().arpeggio)]
+    pub arpeggio: u16,
+
+    /// Order of notes in each chord before applying the arpeggio.
+    #[arg(short, long, value_enum, default_value_t = CliNoteOrder::from(CompileOptions::default().arpeggio_order))]
+    pub order: CliNoteOrder,
+
+    /// Key press duration in milliseconds.
+    #[arg(short, long, default_value_t = CompileOptions::default().click_len)]
+    pub click: u16,
+}
--- crates/y2kx-compiler/src/main.rs
+++ crates/y2kx-compiler/src/main.rs
@@ -1,3 +1,34 @@
+use clap::Parser;
+
+use y2kx::{CompileOptions, NoteOrder, Music};
+use y2kx_compiler::cli::Args;
+
+use midly::Smf;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+
+    let options = CompileOptions {
+        arpeggio: args.arpeggio,
+        arpeggio_order: NoteOrder::from(args.order),
+        click_len: args.click,
+        preparation_time: args.prepare
+    };
+
+
+    let data = std::fs::read(args.input).unwrap();
+    let smf = Smf::parse(&data).unwrap();
+
+    let music = Music::from_smf_range(&smf, 60..=84).unwrap();
+
+    let y2kx = y2kx::compile_with(&music, options).unwrap();
+
+    println!("[INFO] {} tracks:", y2kx.track_count());
+    for track in y2kx.tracks() {
+        println!("[INFO] - {}",track.name);
+    }
+
+    std::fs::write(args.output, y2kx.to_bytes()).unwrap();
+
+    println!("[INFO] y2kx compiled and saved.");
 }
